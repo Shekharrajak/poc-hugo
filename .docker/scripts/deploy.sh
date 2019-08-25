42f1b078559dde082de39782af47a9881d69d26e
shekharrajak <shekharstudy@ymail.com>
2019-08-26 00:02:49 +0530

    travis setup - surge setup - env pass while running the docker

--- .docker/scripts/deploy.sh
+++ .docker/scripts/deploy.sh
@@ -7,8 +7,6 @@ npm install -g surge
 # sh set_env.sh
 # echo "Deploy script has just run set_env script."
 
-echo ${TRAVIS_REPO_SLUG}
-
 # Split on "/", ref: http://stackoverflow.com/a/5257398/689223
 REPO_SLUG_ARRAY=${TRAVIS_REPO_SLUG//\// }
 echo $TRAVIS_REPO_SLUG
@@ -23,23 +21,6 @@ DEPLOY_SUBDOMAIN_UNFORMATTED_LIST=()
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
   DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(pr-${TRAVIS_PULL_REQUEST})
-elif [ -n "${TRAVIS_TAG// }" ] #TAG is not empty
-then
-  #sorts the tags and picks the latest
-  #sort -V does not work on the travis machine
-  #sort -V              ref: http://stackoverflow.com/a/14273595/689223
-  #sort -t ...          ref: http://stackoverflow.com/a/4495368/689223
-  #reverse with sed     ref: http://stackoverflow.com/a/744093/689223
-  #git tags | ignore release candidates | sort versions | reverse | pick first line
-  LATEST_TAG=`git tag | grep -v rc | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | sed '1!G;h;$!d' | sed -n 1p`
-  echo $LATEST_TAG
-  if [ "$TRAVIS_TAG" == "$LATEST_TAG" ]
-  then
-    DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(latest)
-  fi
-  DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_TAG}-tag)
-else
-  DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_BRANCH}-branch)
 fi
 
 
@@ -62,6 +43,7 @@ do
     # Done so because every PR is an issue, and the issues api allows to post general comments,
     # while the PR api requires that comments are made to specific files and specific commits
     GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
+    echo $GITHUB_PR_COMMENTS
     curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Travis automatic deployment: '${DEPLOY_DOMAIN}'"}'
   fi
 done
