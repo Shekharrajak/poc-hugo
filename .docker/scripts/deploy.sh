bdd397578e332bc6ddc977bd4d41a0d5c8ca1e53
shekharrajak <shekharstudy@ymail.com>
2019-08-25 23:58:17 +0530

    travis setup - surge setup - env pass while running the docker

--- .docker/scripts/deploy.sh
+++ .docker/scripts/deploy.sh
@@ -52,7 +52,9 @@ do
   # The length is limited to 253 characters
   # https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax
   DEPLOY_SUBDOMAIN=`echo "$DEPLOY_SUBDOMAIN_UNFORMATTED" | sed -r 's/[\/|\.]+/\-/g'`
+  echo $DEPLOY_SUBDOMAIN
   DEPLOY_DOMAIN=https://${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.surge.sh
+  echo $DEPLOY_DOMAIN
   surge --project ${DEPLOY_PATH} --domain $DEPLOY_DOMAIN;
   if [ "$TRAVIS_PULL_REQUEST" != "false" ]
   then
